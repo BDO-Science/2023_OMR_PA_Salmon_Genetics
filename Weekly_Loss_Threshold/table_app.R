50274bd6e1546e23304ad38e38bcecf3adf2eb7b
bmahardja <brianmahardja@gmail.com>
2023-01-19 16:47:25 -0800

    Updated working shinyapp

--- Weekly_Loss_Threshold/table_app.R
+++ Weekly_Loss_Threshold/table_app.R
@@ -13,13 +13,10 @@ library(DT)
 library(readxl)
 library(lubridate)
 
-genetic_data <-read_excel(file.path("../data_input/WY1996-2022 Genetically Confirmed WR_CVP and SWP salvage and loss for OMR analysis_20230111_Final.xlsx"), sheet = "WY1996-2022 JUVWR Genetic_ID", range = cell_cols("A:K")) %>% 
-    rename(SampleDate='Sample Date',SampleTime='Sample Time',FL=Forklength,Facility=Loc,Salvage_KevinReece=Salvage,Loss_KevinReece=Loss,LAD_KevinReece='Model Race',GeneticAssignment=Assignment) %>%
-    mutate(SampleTime=format(SampleTime, "%H:%M:%S")) %>% mutate(SampleDate=as.Date(SampleDate)) %>%
-    mutate(SampleDateTime=as.POSIXct(paste(SampleDate, SampleTime), format="%Y-%m-%d %H:%M:%S", tz="America/Los_Angeles")) %>% select(-Catch)
+genetic_data <-read.csv("../output/Paired_Genetic_Data_Loss_Comparison_2023-01-17.csv")
 
 genetic_data_sum <- genetic_data %>% mutate(Week=week(SampleDate),Year=year(SampleDate)) %>% filter(Week<=20) %>%
-    group_by(Year,Week) %>% summarise(Loss=sum(Loss_KevinReece))
+    group_by(Year,Week) %>% summarise(Loss=sum(Loss_SalvageData))
     
 dist_data <- read.csv(file = file.path("../data_input/Dummy_dist.csv"))
 
@@ -47,23 +44,31 @@ ui <- fluidPage(
                         "Year",
                         choices=year_options,
                         selected=2019,multiple=FALSE),
-            numericInput("percent_jpe", "% JPE for annual threshold:", 0.54, min = 0.01, max = 100)),
+            numericInput("percent_jpe", "% JPE for annual threshold:", 0.54, min = 0.01, max = 0.99), 
+            helpText("Plot legends: bars = observed weekly loss, red bars = yellow light vulnerability threshold triggered, white solid line = observed accumulated loss, yellow dotted line = yellow light vulnerability threshold, yellow solid line = yellow light cumulative threshold.
+                     NOTE: Yellow light vulnerability and cumulative thresholds are set based on 50% of the JPE annual thresholds.")),
     # Show a plot of the generated distribution
     mainPanel(tabsetPanel(tabPanel("Plot",plotOutput("Plot")),tabPanel("Table",tableOutput("tableSum"))))))
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
     #Pull together data for table
-    sum_data <- reactive({ dist_data %>% left_join(genetic_data_sum %>% filter(Year==as.numeric(input$year_input))) %>% 
-        mutate(Loss=replace_na(Loss,0),Year=as.numeric(input$year_input)) %>%
+    sum_data <- reactive({ dist_data %>% left_join(genetic_data_sum %>% filter(Year==as.integer(input$year_input))) %>% 
+        mutate(Loss=replace_na(Loss,0),Year=as.integer(input$year_input)) %>%
         left_join(jpe_data) %>% mutate(accumulated_loss=cumsum(Loss),
-                                       yellow_vulnerability=JuvenileProductionEstimate*1.17*0.5*Present_inDelta,
-                                       yellow_cumulative=JuvenileProductionEstimate*1.17*0.5*EnterDelta,
-                                       yellow_light_vul=as.factor(ifelse(Loss>yellow_vulnerability,"Yes","No"))) })
+                                       yellow_vulnerability=JuvenileProductionEstimate*(as.numeric(input$percent_jpe)/100)*0.5*Present_inDelta,
+                                       yellow_cumulative=JuvenileProductionEstimate*(as.numeric(input$percent_jpe)/100)*0.5*EnterDelta,
+                                       yellow_light_vul=as.factor(ifelse(Loss>yellow_vulnerability,"Yes","No")),
+                                       yellow_light_cum=as.factor(ifelse(Loss>yellow_cumulative,"Yes","No"))) })
     
     output$Plot <- renderPlot({
-        print(ggplot(data=sum_data()) +
-            geom_bar(aes(x=Week,y=Loss),stat="identity"))
+        print(ggplot(data=sum_data()) + theme_dark() +
+            geom_bar(aes(x=Week,y=Loss,fill=yellow_light_vul),stat="identity")) +
+            scale_x_continuous(breaks=seq(1,18,1),labels = c(1:18)) +
+            geom_line(aes(x=Week,y=accumulated_loss),color="white") +
+            geom_line(aes(x=Week,y=yellow_cumulative),color="yellow",linetype = "solid") +
+            geom_line(aes(x=Week,y=yellow_vulnerability),color="yellow",linetype = "dashed") +
+            scale_fill_manual(name="yellow_light_vul",values=c("black","red"))
     })
     
     output$tableSum <- renderTable(sum_data())
